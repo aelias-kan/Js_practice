be0b6d3efe424a99be11fb58952676545461df27
aelias_kan <milyasstudent@gmail.com>
2025-01-13 14:30:39 +0500

    func&events

---
+++ script/script.js
@@ -0,0 +1,13 @@
+function para_open()
+{
+    tag= document.getElementById("demo");
+    // tag.style.display = "block";
+    tag.className = "open";
+}
+
+function para_close()
+{
+    tag= document.getElementById("demo");
+    // tag.style.display = "none";
+    tag.className = "close";
+}
