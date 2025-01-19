7bc3234bf833414123685a016a7e2e6ef35b353f
aelias_kan <milyasstudent@gmail.com>
2025-01-19 21:49:00 +0500

    Array and for_looping

--- script/script.js
+++ script/script.js
@@ -2,12 +2,36 @@ function para_open()
 {
     tag= document.getElementById("demo");
     // tag.style.display = "block";
+    
     tag.className = "open";
 }
 
 function para_close()
 {
+    
     tag= document.getElementById("demo");
     // tag.style.display = "none";
-    tag.className = "close";
+    
+    tag.className = "closed";
 }
+// Arrays
+var fruits = ["Mango", "Grapes", "pear"];
+function loadfruits()
+{
+    document.getElementById("fruits").innerHTML = fruits;
+}
+
+function myfunction()
+{
+    var frot = prompt("What is ur fav fruit : ");
+
+    fruits[fruits.length] = frot;
+
+    document.getElementById("fruits").innerHTML = fruits;
+    for(let i = 0; i<10; i++)
+    {
+        console.log(i)
+    }
+}
+
+
